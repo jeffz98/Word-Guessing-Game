12744916d6b57fe735ddf21aa42ac2f28007e6d5
Jeffrey <wzhang@wzhangs-MBP.attlocal.net>
2022-07-19 13:19:38 -0700

    passed all Letter testcases

--- Develop/lib/Letter.js
+++ Develop/lib/Letter.js
@@ -1,5 +1,33 @@
 class Letter {
   
+  constructor(letter){
+    const lettersAndNums = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    this.letter = letter;
+    if (lettersAndNums.includes(this.letter)) {
+        this.visible = false;
+    } else {
+        this.visible = true;
+    }
+    
+  }
+  toString() {
+    if (!this.visible) {
+        return '_';
+    } else {
+        return this.letter;
+    }
+  }
+  guess(guessLetter) {
+    if (this.letter === guessLetter) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+
+  getSolution() {
+    return this.letter;
+  }
 }
 
 module.exports = Letter;
